a809e0ee26faa4361c55f2063a84754612ada9da
kenkuts <kennethchoi19@gmail.com>
2020-04-22 22:07:55 +0300

    commit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+require_relative '../lib/user.rb'
 
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize
+        # super
+        @knowledge = []
+    end
+
+    def learn(arr)
+        @knowledge << arr
+    end
 end
